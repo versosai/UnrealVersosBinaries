97c47feea9d8a0d5d447ea0b630c9670191e501e
grzelakc <grzelak@gmail.com>
2024-04-22 09:17:18 -0300

    added v 1.1.0 for Mac

---
+++ V_1.1.0/UE_5.3/Mac/UnrealVersosAI/Source/UnrealVersosAI/Public/ImageCapture.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include "CoreMinimal.h"
+#include "ImageCapture.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGridDimensions
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, Category="Return Values")
+	int32 MaxX;
+
+	UPROPERTY(BlueprintReadWrite, Category="Return Values")
+	int32 MaxY;
+};
+
+FGridDimensions make_waffle_image(::std::string path_to_image, ::std::string path_to_output_image, int cell_size);
+TArray<FString> split_image_into_quads(const FString& inputPathFstr);
+FIntPoint get_image_size(const FString& InputPath);
+int32 crop_to_square(const FString& InputPath);
