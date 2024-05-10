0383dba7114df2d099e076956d6356603ba34850
grzelakc <grzelak@gmail.com>
2024-05-10 15:42:33 -0300

    Version 1.2 for Mac for UE 5.3

---
+++ V_1.2.0/UE_5.3/Mac/UnrealVersosAI/Intermediate/Build/Mac/UnrealEditor/Inc/UnrealVersosAI/UHT/UnrealVersosAI.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeUnrealVersosAI_init() {}
+	UNREALVERSOSAI_API UFunction* Z_Construct_UDelegateFunction_UnrealVersosAI_OnFileDownloadComplete__DelegateSignature();
+	UNREALVERSOSAI_API UFunction* Z_Construct_UDelegateFunction_UnrealVersosAI_OnImagePostComplete__DelegateSignature();
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_UnrealVersosAI;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_UnrealVersosAI()
+	{
+		if (!Z_Registration_Info_UPackage__Script_UnrealVersosAI.OuterSingleton)
+		{
+			static UObject* (*const SingletonFuncArray[])() = {
+				(UObject* (*)())Z_Construct_UDelegateFunction_UnrealVersosAI_OnFileDownloadComplete__DelegateSignature,
+				(UObject* (*)())Z_Construct_UDelegateFunction_UnrealVersosAI_OnImagePostComplete__DelegateSignature,
+			};
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/UnrealVersosAI",
+				SingletonFuncArray,
+				UE_ARRAY_COUNT(SingletonFuncArray),
+				PKG_CompiledIn | 0x00000040,
+				0x83B16038,
+				0xADB9BB1E,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_UnrealVersosAI.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_UnrealVersosAI.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UnrealVersosAI(Z_Construct_UPackage__Script_UnrealVersosAI, TEXT("/Script/UnrealVersosAI"), Z_Registration_Info_UPackage__Script_UnrealVersosAI, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x83B16038, 0xADB9BB1E));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
